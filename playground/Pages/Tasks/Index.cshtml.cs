89fd9d6d6d13da8cd74b00c723a833c2977dc31b
aserowy <serowy@hotmail.com>
2023-12-09 13:46:25 +0100

    added empty tasks page
    Signed-off-by: aserowy <serowy@hotmail.com>

---
+++ playground/Pages/Tasks/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace playground.Pages.Tasks
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
